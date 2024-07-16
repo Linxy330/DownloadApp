3e416c981715a64bda54c2de3d74a8ca75a0b566
KennyLin <linxinyu330@gmail.com>
2024-07-16 22:38:19 +0800

    Del some unused code

--- app/src/main/java/com/kenny/downloadapp/MainActivity.java
+++ app/src/main/java/com/kenny/downloadapp/MainActivity.java
@@ -18,13 +18,11 @@ import androidx.core.content.ContextCompat;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
